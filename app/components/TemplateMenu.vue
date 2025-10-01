3f8caa0f5a68a47016c519a3400ded4a2a53a3b1
DBouraoui <contact@dbouraoui.fr>
2025-10-01 14:55:04 +0200

    Major: V1

---
+++ app/components/TemplateMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <UDropdownMenu
+    v-slot="{ open }"
+    :modal="false"
+    :items="[{
+      label: 'Alfa 1.0'
+    }]"
+    :content="{ align: 'start' }"
+    :ui="{ content: 'min-w-fit' }"
+    size="xs"
+  >
+    <UButton
+      label="Version"
+      variant="subtle"
+      trailing-icon="i-lucide-chevron-down"
+      size="xs"
+      class="-mb-[6px] font-semibold rounded-full truncate"
+      :class="[open && 'bg-primary/15']"
+      :ui="{
+        trailingIcon: ['transition-transform duration-200', open ? 'rotate-180' : undefined].filter(Boolean).join(' ')
+      }"
+    />
+  </UDropdownMenu>
+</template>
